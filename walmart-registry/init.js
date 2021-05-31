342973e35ad8de165708b251d3ad993a3c19cfba
fortdev210 <sharp.blader2050@gmail.com>
2021-05-31 17:19:51 +0200

    add status handler

--- walmart-registry/init.js
+++ walmart-registry/init.js
@@ -7,6 +7,12 @@ async function main() {
     const dbInstance = new STLPRO_MANAGER();
     const flagInstance = new Flag_Order();
     const dsOrders = await dbInstance.getOrders();
+    console.log(`Get ${dsOrders.length} ds orders in total.`.bgGreen);
+    for (let i =0; i < dsOrders.length; i++) {
+        const dbInstance = new STLPRO_MANAGER(dsOrders[i]);
+        const currentOrderInfo = await dbInstance.getOrderDetails();
+        console.log(currentOrderInfo)
+    }
 }
 
 main();
