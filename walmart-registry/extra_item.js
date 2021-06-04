43b46bf634230ddf850687af635c34f2ea5c3d45
xiaoping-stlpro <xiaoping@stlpro.com>
2021-06-04 13:27:33 -0400

    register customer information

--- walmart-registry/extra_item.js
+++ walmart-registry/extra_item.js
@@ -63,16 +63,140 @@ class ExtraItemHandler extends PuppeteerBase {
 
   async signInWalmart() {
     try {
-      await this.fillNewSignInForm();
-    } catch (error) {
+      await this.waitForLoadingElement('#password');
       console.log("Old Signin Page...");
       await this.fillOldSignInForm();
+    } catch (error) {
+      await this.fillNewSignInForm();
     }
+  }
+
+  scheduleDate(){
+    const now = Date.parse(new Date());
+    const schedule = new Date(now + 1000 * 24 * 3600 * 14);
+    const scheduleDate =
+      schedule.getMonth() +
+      1 +
+      "/" +
+      (schedule.getDate() < 10 ? "0" + schedule.getDate() : schedule.getDate()) +
+      "/" +
+      schedule.getFullYear();
+    return scheduleDate
+  }
+
+  async addEventDate() {
+    const eventDate = this.scheduleDate();
+    try {
+      await this.waitForLoadingElement('#eventDate');
+      await this.insertValue('#eventDate', eventDate)
+    } catch (error) {
+      console.log("Error while loading the page, should reload.");
+      await this.page.reload();
+      await this.waitForLoadingElement('#eventDate');
+      await this.insertValue('#eventDate', eventDate)
+    }
+  }
+
+  async addLocation() {
+    const state = this.getFullState(this.customerInfo.state);
+    await this.waitForLoadingElement('#regstate');
+    await this.insertValue('#regstate', state);
+  }
+
+  async addOrganization() {
+    await this.waitForLoadingElement('#lastname');
+    await this.reInsertValue('#lastname', this.customerInfo.lastName);
+  }
+
+  async removeOldAddress() {
+    try {
+      await this.waitForLoadingElement('[class="shipping-address-footer"]');
+      await this.clickButton('[class*="delete-button "]')
+    } catch (error) {
+      console.log("No old address exists!");
+    }
+  }
+
+  async addPersonalInfo() {
+    try {
+      await this.waitForLoadingElement('#firstName');
+    } catch (error) {
+      await this.page.reload();
+      await this.addEventDate();
+      await this.addLocation();
+      await this.addOrganization();
+      await this.removeOldAddress();
+    }
+    await this.insertValue('#firstName', this.customerInfo.firstName);
+    await this.insertValue('#lastName', this.customerInfo.lastName);
+    await this.insertValue('#phone', this.customerInfo.phoneNum);
+    await this.insertValue('#addressLineOne', this.customerInfo.addressOne);
+    await this.insertValue('#addressLineTwo', this.customerInfo.addressTwo);
+    await this.insertValue('#city', this.customerInfo.city);
+    await this.insertValue('#state', this.customerInfo.state);
+    await this.insertValue('#postalCode', this.customerInfo.zipCode);
+    await this.clickButton('[data-automation-id="address-form-submit"]')
+    console.log('All information successfully registered.')
+  }
+
+  async registerCustomerInfo() {
+    await this.addEventDate();
+    await this.addLocation();
+    await this.addOrganization();
+    await this.removeOldAddress();
+    await this.addPersonalInfo();
+  }
+
+  async verifyAddress() {
+    try {
+      await this.waitForLoadingElement('[class*="alert-warning"]');
+      await this.clickButton('[data-automation-id="address-form-submit"]');
+    } catch (error) {
+      console.log("No address warning");
+    }
+    try {
+      await this.waitForLoadingElement('[class="validation-wrap"]', 20000);
+      console.log("Verify address");
+      await this.clickButton('[class*="button-save-address"]');
+    } catch (error) {
+      console.log("No address error");
+    }
+  }
+
+  async makeRegistryPublic() {
+    console.log("Make my registry public");
+    await this.page.evaluate(() => {
+      document
+        .querySelectorAll(".LNR-PrivacyOptions")[0]
+        .childNodes[1].querySelector("input")
+        .click();
+    });
+    await this.sleep(3000);
+    ///--- Click looking good button ---///
+    console.log("Click looking good button");
+    await this.page.evaluate(() => {
+      document
+        .querySelector('[class="m-padding-ends"]')
+        .querySelector("button")
+        .click();
+    });
   }
 
   async process() {
     await this.goSignInPage();
     await this.signInWalmart();
+    const captchaDetected = await this.checkCaptcha(5000);
+    if (captchaDetected) {
+      console.log('Use another proxy to bypass captcha...')
+      // await this.luminatiProxyManager('OFF');
+      // await this.luminatiProxyManager('ON');
+      await this.page.reload();
+      await this.signInWalmart();
+    } 
+    console.log('Successfully signed in, registering...');
+    await this.registerCustomerInfo();
+    await this.verifyAddress();
+    await this.makeRegistryPublic();
   }
 }
 
