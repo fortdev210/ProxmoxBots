deef1b210b1fd688f559fc98d35aa4f8b16d2d95
fortdev210 <sharp.blader2050@gmail.com>
2021-06-09 21:35:43 +0200

    add apis and initial set for wm buybot

---
+++ walmart-buy/init.js
@@ -0,0 +1,16 @@
+const colors = require("colors");
+const STLPRO_MANAGER = require("../lib/stlpro");
+const Flag_Order = require("../lib/flag");
+/////////////
+// WELCOME //
+/////////////
+console.log(" Walmart Buybot                                ".inverse.bold);
+console.log(" Property of STL Pro, Inc.                     ".inverse);
+console.log(" Developed by Xiaoping Jin                     ".inverse);
+console.log(" For tips, please run the bot by executing:    ".inverse);
+console.log(" node init.js                                  ".inverse);
+console.log("");
+
+async function main() {}
+
+main();
