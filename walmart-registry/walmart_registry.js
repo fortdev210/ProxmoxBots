a1c9463526973f18ba84f856452adffcf40a17ce
xiaoping-stlpro <xiaoping@stlpro.com>
2021-07-12 15:11:32 -0400

    fix empty target

--- walmart-registry/walmart_registry.js
+++ walmart-registry/walmart_registry.js
@@ -297,13 +297,13 @@ class WalmartRegistry extends PuppeteerBase {
   }
 
   async extraItemProcess() {
-    await this.clearSiteSettings()
     await this.luminatiProxyManager("ON", [
       this.customerInfo.ip,
       this.customerInfo.port,
     ]);
     await this.sleep(3000);
     await this.goSignInPage();
+    await this.clearSiteSettings()
     await this.signInWalmart();
     const captchaDetected = await this.checkCaptcha(5000);
     if (captchaDetected) {
@@ -337,13 +337,13 @@ class WalmartRegistry extends PuppeteerBase {
   }
 
   async noExtraItemProcess() {
-    await this.clearSiteSettings()
     await this.luminatiProxyManager("ON", [
       this.customerInfo.ip,
       this.customerInfo.port,
     ]);
     await this.sleep(3000);
     await this.goSignUpPage();
+    await this.clearSiteSettings()
     await this.fillSignUpForm();
     const captchaDetected = await this.checkCaptcha(5000);
     if (captchaDetected) {
