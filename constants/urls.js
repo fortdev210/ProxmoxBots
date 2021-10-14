53b54d3e27efa5058d0a90ad4e28de96de78099a
xiaoping-stlpro <xiaoping@stlpro.com>
2021-10-14 17:51:54 -0400

    add logger and base walmart

---
+++ constants/urls.js
@@ -0,0 +1,4 @@
+const WALMART = {
+  REGISTRY_EVENT_LINK:
+    "https://www.walmart.com/account/login?returnUrl=%2Flists%2Fcreate-events-registry%3Fr%3Dyes",
+};
---
+++ new-walmart-registry/init.js
@@ -0,0 +1,1 @@
+const LOGGER = require("../lib/logger");
