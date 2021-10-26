20a1a694122d1255e6fcb1a2795996cf4ad40e68
xiaoping-stlpro <xiaoping@stlpro.com>
2021-10-26 16:58:31 -0400

    add init setup

--- new-walmart-buy/init.js
+++ new-walmart-buy/init.js
@@ -1,4 +1,15 @@
 const WalmartBuy = require("./walmart_buy");
+require("dotenv").config();
+const prefix = "Walmart+Pp";
+const botNumber =
+  process.env.TEST_MODE === "true"
+    ? 3
+    : require("os").hostname().replace(/\D/g, "").replace("0", "");
+const flag = prefix + botNumber;
+const API = require("../lib/api");
+const LOGGER = require("../lib/logger");
+const { parseDSOrderInfo } = require("../lib/utils");
+const { WAREHOUSE_BUY_BOTS_NUMBERS } = require("../constants/warehouse");
 
 async function main() {
   const buyer = new WalmartBuy();
